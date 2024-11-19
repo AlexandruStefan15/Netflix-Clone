9d6023119b2606a40c87a283775ec0691c38134a
Alex Nastase <alexandru.stefan.nastase@gmail.com>
2024-11-19 20:59:25 +0200

    Normalize line endings

--- src/Assets/gifs/gifs.js
+++
@@ -1,7 +0,0 @@
-import downloadIcon from "./download-icon.gif";
-
-const gifs = {
-	downloadIcon,
-};
-
-export default gifs;
--- src/Assets/videos/videos.js
+++
@@ -1,7 +0,0 @@
-import videoStory1 from "./videoStory1.m4v";
-import strangerThingsGuys from "./strangerThingsGuys.m4v";
-
-export default {
-	videoStory1,
-	strangerThingsGuys,
-};
