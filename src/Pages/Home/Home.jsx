c1c399c1fbd9d6ae3c5954f5d972fa5c69824b3e
Alex Nastase <alexandru.stefan.nastase@gmail.com>
2024-12-13 00:03:11 +0200

    good

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import styles from "./Home.module.scss";
 
 import Header from "../../Components/Header/Header";
-import Footer from "../../Components/Footer/Footer";
 import HeroBanner from "../../Components/Sections/HeroBanner/HeroBanner";
-import Faq from "../../Components/Sections/Faq/Faq";
 import StoryPanel from "../../Components/Sections/StoryPanel/StoryPanel";
 import StoryPanel2 from "../../Components/Sections/StoryPanel2/StoryPanel2";
 import StoryPanel3 from "../../Components/Sections/StoryPanel3/StoryPanel3";
 import StoryPanel4 from "../../Components/Sections/StoryPanel4/StoryPanel4";
+import Faq from "../../Components/Sections/Faq/Faq";
+import Footer from "../../Components/Footer/Footer";
 
 export default function Home() {
 	return (
